9794041e153118322cd920d78114bec99fafca8b
chaschev <chaschev@gmail.com>
2013-09-30 18:52:37 +0400

    Added Install Mojo.

---
+++ src/main/java/com/chaschev/AbstractExecMojo2.java
@@ -0,0 +1,234 @@
+package com.chaschev;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
+import org.apache.commons.lang3.SystemUtils;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.codehaus.plexus.util.StringUtils;
+import org.codehaus.plexus.util.cli.CommandLineUtils;
+import org.eclipse.aether.RepositorySystem;
+import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.artifact.Artifact;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.collection.CollectRequest;
+import org.eclipse.aether.graph.Dependency;
+import org.eclipse.aether.graph.DependencyFilter;
+import org.eclipse.aether.repository.LocalRepository;
+import org.eclipse.aether.repository.RemoteRepository;
+import org.eclipse.aether.repository.RepositoryPolicy;
+import org.eclipse.aether.resolution.*;
+import org.eclipse.aether.util.artifact.JavaScopes;
+import org.eclipse.aether.util.filter.DependencyFilterUtils;
+import org.eclipse.aether.version.Version;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Andrey Chaschev chaschev@gmail.com
+ */
+public abstract class AbstractExecMojo2 extends AbstractMojo {
+    private static final Pattern ALT_REPO_SYNTAX_PATTERN = Pattern.compile("(.+)::(.*)::(.+)");
+
+    /**
+     * Artifact to execute. I.e. com.chaschev:cap4j:1.0 or com.chaschev.
+     */
+    @Parameter(property = "artifact", required = true)
+    protected String artifactName;
+
+    @Parameter(property = "class")
+    protected String className;
+
+    @Parameter(property = "version", defaultValue = "LATEST")
+    protected String artifactVersion;
+
+    /**
+     * Include snapshots releases when version is not set.
+     */
+    @Parameter(property = "snapshots", defaultValue = "false")
+    protected boolean snapshots;
+
+    /**
+     * Repositories in the format id::[layout]::url or just url, separated by comma.
+     * ie. central::default::http://repo1.maven.apache.org/maven2,myrepo::::http://repo.acme.com,http://repo.acme2.com
+     */
+    @Parameter(property = "remoteRepositories", defaultValue =
+        "http://repo1.maven.apache.org/maven2," +
+            "https://oss.sonatype.org/content/repositories/snapshots")
+    protected String remoteRepositories;
+
+    /**
+     * Arguments for the executed program
+     */
+    @Parameter(property = "args")
+    protected String commandlineArgs;
+    @Parameter
+    protected Property[] systemProperties;
+
+    @Parameter(property = "forceDownload", defaultValue = "false")
+    private boolean forceDownload;
+
+    @Parameter(property = "localRepo")
+    protected String localRepo;
+
+    protected List<RemoteRepository> repositories;
+    protected RepositorySystem system;
+    protected RepositorySystemSession session;
+
+    protected void initialize() throws VersionRangeResolutionException, MojoFailureException {
+        File repositoryFile = localRepo == null ? (new File(SystemUtils.getUserHome(),
+              ".m2/repository")) : new File(localRepo);
+
+        Preconditions.checkArgument(repositoryFile.exists(), "could not find local repo at: %s", repositoryFile.getAbsolutePath());
+
+        LocalRepository localRepository = new LocalRepository(repositoryFile);
+
+        repositories = new RepositoryParser().parse(remoteRepositories);
+
+        system = Booter.newRepositorySystem();
+        session = Booter.newRepositorySystemSession(system, localRepository);
+
+        if ("LATEST".equals(artifactVersion)) {
+            Artifact artifact = new DefaultArtifact(artifactName + ":[0,)");
+
+            VersionRangeRequest rangeRequest = new VersionRangeRequest();
+
+            rangeRequest.setArtifact( artifact );
+            rangeRequest.setRepositories(repositories);
+
+
+            VersionRangeResult rangeResult = system.resolveVersionRange(session, rangeRequest);
+
+            List<Version> versions = Lists.reverse(rangeResult.getVersions());
+
+//            Version matchedVersion = rangeRequest.;
+            Version matchedVersion = null;
+
+            if(!snapshots) {
+                for (int i = 0; i < versions.size(); i++) {
+                    Version version = versions.get(i);
+                    if (!version.toString().toUpperCase().contains("SNAPSHOT")) {
+                        matchedVersion = version;
+                        break;
+                    }
+                }
+                if(matchedVersion == null){
+                    matchedVersion = rangeResult.getHighestVersion();
+                    if(matchedVersion != null){
+                        getLog().warn("didn't find non-snapshot version for " + artifactName +
+                            ", using snapshot version: " + matchedVersion);
+                    }
+                }
+            }else{
+                matchedVersion = rangeResult.getHighestVersion();
+            }
+
+            if(matchedVersion == null){
+                throw new MojoFailureException("didn't find matching version for " + artifactName);
+            }
+
+            artifactVersion = matchedVersion.toString();
+        }
+
+        artifactName += ":" + artifactVersion;
+    }
+
+    protected Artifact resolveArtifact(String artifactToResolve) throws ArtifactResolutionException {
+        getLog().info("resolving artifact " + artifactToResolve);
+
+        Artifact artifact = new DefaultArtifact(artifactToResolve);
+
+        ArtifactRequest artifactRequest = new ArtifactRequest();
+        artifactRequest.setArtifact(artifact);
+
+        artifactRequest.setRepositories(repositories);
+
+        ArtifactResult artifactResult = system.resolveArtifact(session, artifactRequest);
+
+        return artifactResult.getArtifact();
+    }
+
+    protected List<ArtifactResult> getDependencies(Artifact artifact) throws DependencyResolutionException {
+        DependencyFilter dependencyFilter = DependencyFilterUtils.classpathFilter(JavaScopes.COMPILE);
+
+        CollectRequest collectRequest = new CollectRequest();
+        collectRequest.setRoot( new Dependency( artifact, JavaScopes.COMPILE ) );
+        collectRequest.setRepositories(repositories);
+
+        DependencyRequest dependencyRequest = new DependencyRequest( collectRequest, dependencyFilter );
+
+        return system.resolveDependencies(session, dependencyRequest).getArtifactResults();
+    }
+
+    protected String[] parseArgs() throws MojoExecutionException {
+        if (commandlineArgs == null) return null;
+
+        try {
+            return CommandLineUtils.translateCommandline(commandlineArgs);
+        } catch (Exception e) {
+            throw new MojoExecutionException(e.toString());
+        }
+    }
+
+    public static class RepositoryParser {
+
+        public RepositoryParser() {
+        }
+
+        public List<RemoteRepository> parse(String repos) {
+            try {
+                List<RemoteRepository> repoList = new ArrayList<RemoteRepository>();
+
+                for (String repo : repos.split(",")) {
+                    repoList.add(parseRepository(repo));
+                }
+
+                return repoList;
+            } catch (MojoFailureException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public RemoteRepository parseRepository(String repo)  throws MojoFailureException {
+            // if it's a simple url
+            String id = null;
+            String layout = "default";
+            String url = repo;
+
+            // if it's an extended repo URL of the form id::layout::url
+            if (repo.contains("::")) {
+                Matcher matcher = ALT_REPO_SYNTAX_PATTERN.matcher(repo);
+
+                if (!matcher.matches()) {
+                    throw new MojoFailureException(repo, "Invalid syntax for repository: " + repo,
+                        "Invalid syntax for repository. Use \"id::layout::url\" or just \"URL\".");
+                }
+
+                id = matcher.group(1).trim();
+
+                if (!StringUtils.isEmpty(matcher.group(2))) {
+                    layout = matcher.group(2).trim();
+                }
+
+                url = matcher.group(3).trim();
+            } else {
+                url = repo;
+            }
+
+            RemoteRepository.Builder builder = new RemoteRepository.Builder(id, layout, url);
+
+            if(url.contains("snapshot")){
+                builder.setSnapshotPolicy(new RepositoryPolicy(true, RepositoryPolicy.UPDATE_POLICY_ALWAYS,
+                    RepositoryPolicy.CHECKSUM_POLICY_WARN));
+            }
+
+            return builder.build();
+        }
+    }
+}
--- src/main/java/com/chaschev/InstallMojo.java
+++ src/main/java/com/chaschev/InstallMojo.java
@@ -16,74 +16,171 @@ package com.chaschev;
  * limitations under the License.
  */
 
-import org.apache.maven.execution.MavenSession;
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.BuildPluginManager;
+import com.chaschev.chutils.util.OpenBean2;
+import com.google.common.base.Optional;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.SystemUtils;
+import org.apache.maven.DefaultMaven;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.project.MavenProject;
-import org.codehaus.plexus.configuration.xml.XmlPlexusConfiguration;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.eclipse.aether.artifact.Artifact;
+import org.eclipse.aether.resolution.ArtifactResult;
 
-/**
- * Goal which touches a timestamp file.
- *
- * @goal install
- * @phase install
- */
-public class InstallMojo extends AbstractMojo {
-    /**
-     * Plugin to execute.
-     *
-     * @parameter
-     */
-    private String pluginString;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-    /**
-     * Plugin goal to execute.
-     *
-     * @parameter
-     */
-    private String goal;
+@Mojo(name = "install", requiresProject = false, threadSafe = true)
+public class InstallMojo extends AbstractExecMojo2 {
+    public static final class MatchingPath implements Comparable<MatchingPath>{
+        boolean isJDK;
 
-    /**
-     * Plugin configuration to use in the execution.
-     *
-     * @parameter
-     */
-    private XmlPlexusConfiguration configuration;
+        String path;
 
-    /**
-     * The project currently being build.
-     *
-     * @parameter expression="${project}"
-     * @required
-     * @readonly
-     */
-    private MavenProject mavenProject;
+        public MatchingPath(boolean JDK, String path) {
+            isJDK = JDK;
+            this.path = path;
+        }
 
-    /**
-     * The current Maven session.
-     *
-     * @parameter expression="${session}"
-     * @required
-     * @readonly
-     */
-    private MavenSession mavenSession;
+        @Override
+        public int compareTo(MatchingPath o) {
+            if(isJDK && !o.isJDK) return 1;
+            if(!isJDK && o.isJDK) return -1;
 
-    /**
-     * The Maven BuildPluginManager component.
-     *
-     * @component
-     * @required
-     */
-    private BuildPluginManager pluginManager;
-
-
-    //Plan:
-
-
-    //link: com.chaschev:cap4:1.0-SNAPSHOT:com.chaschev.cap4.main.Cap4j
-    //execute com.chaschev:cap4:1.0-SNAPSHOT:com.chaschev.cap4.main.Cap4j
-
-    public void execute() throws MojoExecutionException {
+            return path.compareTo(o.path);
+        }
     }
+
+    @Parameter(property = "installTo")
+    private String installTo;
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        try {
+            initialize();
+
+            Artifact artifact = resolveArtifact(artifactName);
+
+            if(className != null){
+                List<ArtifactResult> artifactResults = getDependencies(artifact);
+                new ExecObject2(getLog(),
+                    artifact, artifactResults, className,
+                    parseArgs(),
+                    systemProperties
+                ).execute();
+            }
+
+            Class<?> installation = new URLClassLoader(new URL[]{artifact.getFile().toURI().toURL()}).loadClass("Installation");
+
+            List<Object[]> entries = (List<Object[]>) OpenBean2.getStaticFieldValue(installation, "shortcuts");
+
+            if(installTo == null){
+               installTo = findPath();
+            }
+
+            File installToDir = new File(installTo);
+
+            for (Object[] entry : entries) {
+                String shortCut = (String) entry[0];
+                String className = ((Class)entry[1]).getName();
+
+
+                File file;
+                if(SystemUtils.IS_OS_WINDOWS){
+
+                    FileUtils.writeStringToFile(
+                        file = new File(installToDir, shortCut + ".bat"),
+                        String.format("@mvn -q installation:exec -Dartifact=%s:%s -Dversion=%s -Dclass=%s -Dargs=\"%s\"",
+                            artifact.getGroupId(),
+                            artifact.getArtifactId(),
+                            artifact.getBaseVersion(),
+                            className,
+                            "%*")
+                    );
+                }else{
+                    FileUtils.writeStringToFile(
+                        file = new File(installToDir, shortCut + ".sh"),
+                        String.format("mvn -q installation:exec -Dartifact=%s:%s -Dversion=%s -Dclass=%s -Dargs=\"%s\"",
+                            artifact.getGroupId(),
+                            artifact.getArtifactId(),
+                            artifact.getBaseVersion(),
+                            className,
+                            "$*")
+                    );
+                }
+
+                getLog().info("created shortcut: " + shortCut + " -> " + file.getAbsolutePath());
+            }
+        } catch (Exception e) {
+            if(e instanceof RuntimeException){
+                throw (RuntimeException)e;
+            }else{
+                getLog().error(e.toString(), e);
+                throw new MojoExecutionException(e.toString());
+            }
+        }
+    }
+
+    private String findPath() throws MojoFailureException {
+        String path = Optional.fromNullable(System.getenv("path")).or(System.getenv("PATH"));
+
+        String[] pathEntries = path == null ? new String[0] : path.split(File.pathSeparator);
+
+        String javaHomeAbsPath = SystemUtils.getJavaHome().getParentFile().getAbsolutePath();
+
+        File mavenHome = new File(DefaultMaven.class.getProtectionDomain().getCodeSource().getLocation().getFile()).getParentFile().getParentFile();
+        String mavenHomeAbsPath = mavenHome.getAbsolutePath();
+
+        List<MatchingPath> matchingPaths = new ArrayList<MatchingPath>();
+
+        for (String pathEntry : pathEntries) {
+            File entryFile = new File(pathEntry);
+            String absPath = entryFile.getAbsolutePath();
+
+            boolean writable = isWritable(entryFile);
+
+            if(absPath.startsWith(javaHomeAbsPath)){
+                if(!writable){
+                    getLog().warn(absPath + " is not writable");
+                }else{
+                    matchingPaths.add(new MatchingPath(true, absPath));
+                }
+            } else
+            if(absPath.startsWith(mavenHomeAbsPath)){
+                if(!writable){
+                    getLog().warn(absPath + " is not writable");
+                }else{
+                    matchingPaths.add(new MatchingPath(false, absPath));
+                }
+            }
+        }
+
+        Collections.sort(matchingPaths);
+
+        if(matchingPaths.isEmpty()){
+            throw new MojoFailureException("could not find a bin folder to write to");
+        }
+
+        return matchingPaths.get(0).path;
+    }
+
+    private static boolean isWritable(File dir) {
+        try {
+            File tempFile = File.createTempFile("testWrite", "txt", dir);
+            if(tempFile.exists()) {
+                tempFile.delete();
+                return true;
+            }
+        } catch (IOException e) {
+            return false;
+        }
+
+        return dir.canWrite();
+    }
+
 }
